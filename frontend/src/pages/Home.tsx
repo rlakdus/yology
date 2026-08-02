1af3ffeafe6d7ddd73206fd88679a9fbfc0d7ddb
anstjgus922 <msh176125@naver.com>
2026-08-02 16:38:24 +0900

    feat: initialize frontend routing and home page

---
+++ frontend/src/pages/Home.tsx
@@ -0,0 +1,12 @@
+const Home = () => {
+  return (
+    <div>
+      <h1>FeelBack</h1>
+      <p>Emotion Reconstruction System</p>
+
+      <button>Start</button>
+    </div>
+  );
+};
+
+export default Home;
